75a9f9601877f4a05f1c786283389266c0f74c6f
AmolSamota <samota.amol.21apr@gmail.com>
2021-06-12 12:00:14 +0530

    #22 payment screen made

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -21,6 +21,9 @@ const initialState = {
       ? JSON.parse(localStorage.getItem("cartItems"))
       : [],
   },
+  shippingAddress: localStorage.getItem("shippingAddress")
+    ? JSON.parse(localStorage.getItem("shippingAddress"))
+    : {},
 };
 const reducer = combineReducers({
   productList: productListReducer,
